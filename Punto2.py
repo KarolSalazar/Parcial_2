84f478331a37d27caa3cd829f77ffee3f0729ebc
KarolSalazar <karolv.salazarq@uqvirtual.edu.co>
2023-05-05 14:11:35 -0500

    Punto 2 funcionando correctamente

--- Punto2.py
+++ Punto2.py
@@ -1,3 +1,26 @@
+import sqlalchemy
+import pandas as pd
+
+
 class Ejercicio2:
     def __init__(self):
-        pass
+        self.dbEngine = self.crear_engine()
+
+    def consulta_sql(self):
+        consulta = input("Ingrese la consulta a realizar:")
+        try:
+            dataframe = pd.read_sql(consulta, self.dbEngine)
+            print("Consulta realizada: ", consulta,
+                  "\nResultado obtenido: \n", dataframe)
+        except:
+            print("Algo ha salido mal con la consulta, revisa que esté bien redactada, o que la base dedatos si contenga dicha información solicitada")
+
+    def crear_engine(self):
+        dbEngine = sqlalchemy.create_engine(
+            'sqlite:///BaseDatosParcial2.sqlite')
+
+        return dbEngine
+
+
+lector_tablas = Ejercicio2()
+lector_tablas.consulta_sql()
